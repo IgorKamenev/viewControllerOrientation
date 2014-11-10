2a7cb3b8b1bb6b702fbd7715ab21b8e7e513351e
Igor Kamenev <igorkamenev@yandex.ru>
2014-11-10 16:58:31 +0300

    Initial Commit

---
+++ Orientations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Orientations
+//
+//  Created by Igor Kamenev on 10/11/14.
+//  Copyright (c) 2014 Igor Kamenev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
